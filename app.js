42625ec36441455a552ca5bd4d0ba9b1ebfe8e59
Shshank <shshank.jha.0805@gmail.com>
2026-02-17 14:58:29 +1100

    working run

--- app.js
+++ app.js
@@ -3,23 +3,48 @@ const strtbtn = document.querySelector(".b-start")
 const stopbtn = document.querySelector(".b-stop")
 const minutes = document.querySelector(".minute")
 const seconds = document.querySelector(".seconds")
-const easy = document.querySelector(".button1 light")
-const moderate = document.querySelector(".button1 moderate")
-const intensive = document.querySelector(".button1 intense")
+const easy = document.querySelector(".button1.light")
+const moderate = document.querySelector(".button1.moderate")
+const intensive = document.querySelector(".button1.intense")
 
 let totalseconds = 0 
 let running = false
 let split = [25,5]
+let myInterval = null 
+let phase = 0
 
 function splitchange(x,y){
-    if (running === True) {
+    if (running === true) {
         return
     } else {
         split = [x,y]
+        minutes.textContent = x 
     }
 }
 
-function updatetimer(){ 
+function nextPhase(){
+    bells.play();
+    clearInterval(myInterval)
+    phase++;
+
+    if (phase >= split.length) {
+        running = false;
+        clearInterval(myInterval);
+    } else { 
+        
+        running = false;
+        start();
+    }
+}
+
+function updatetimer(){
+    if (totalseconds <= 0){
+        nextPhase();
+        return;
+    } 
+
+    totalseconds --;
+
     const minuteleft = Math.floor(totalseconds/60);
     const secondsleft = totalseconds%60;
 
@@ -30,27 +55,17 @@ function updatetimer(){
     } else {
         seconds.textContent = secondsleft
     }
-
-    if (minuteleft === 0 && secondsleft ===0){
-        bells.play()
-        clearInterval(myInterval)
-    }
 }
-
 
 function start(){
-    running = True; 
-
-    for (let i = 0; i < split.length; i++) {
-        totalseconds = split[i] * 60 
-
-        myInterval = setInterval(updatetimer, 1000)
-    }
-
+    if (running) return;
+    running = true; 
+    totalseconds = split[phase] * 60 
+    myInterval = setInterval(updatetimer, 1000)
 }
 
-easy.addEventListener("click", splitchange(25,5))
-moderate.addEventListener("click", splitchange(45,15))
-intensive.addEventListener("click", splitchange(52,8))
-strtbtn.addEventListener("click", start())
+easy.addEventListener("click", () =>splitchange(25,5))
+moderate.addEventListener("click", () =>splitchange(45,15))
+intensive.addEventListener("click", () => splitchange(52,8))
+strtbtn.addEventListener("click", start)
 
